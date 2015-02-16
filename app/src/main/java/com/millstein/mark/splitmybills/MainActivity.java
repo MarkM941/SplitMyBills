8631e48ab72d1771b25b1384fda7767fd8bd085f
Mark <m.millstein1234@gmail.com>
2015-02-16 18:00:44 -0500

    Added basic ExpandableList

--- app/src/main/java/com/millstein/mark/splitmybills/MainActivity.java
+++ app/src/main/java/com/millstein/mark/splitmybills/MainActivity.java
@@ -5,29 +5,85 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import com.millstein.mark.splitmybills.math.Algorithms;
 import com.millstein.mark.splitmybills.math.Triple;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+    
+    ExpandableListView listView;
+    ExpandableListAdapter listAdapter;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Triple result = Algorithms.extendedEuclid(5, 1);
-        Log.v("blah", result.toString());
-        String out = "";
-        for(int i = 1; i < 4; i++) {
-            out += result.toString() + "\n";
-            result = Algorithms.generateNewGCDSolution(1, 5, i, result);
-        }
+        listView = (ExpandableListView) findViewById(R.id.player_list);
+        prepareListData();
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        listView.setAdapter(listAdapter);
+        
+        
+//        Triple result = Algorithms.extendedEuclid(5, 1);
+//        Log.v("blah", result.toString());
+//        String out = "";
+//        for(int i = 1; i < 4; i++) {
+//            out += result.toString() + "\n";
+//            result = Algorithms.generateNewGCDSolution(1, 5, i, result);
+//        }
+//
+//        TextView textView = (TextView) findViewById(R.id.myTextView);
+//        textView.setText(out);
+    }
+    
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
 
-        TextView textView = (TextView) findViewById(R.id.myTextView);
-        textView.setText(out);
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+        
     }
 
 
@@ -43,12 +99,12 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/millstein/mark/splitmybills/Person.java
+++ app/src/main/java/com/millstein/mark/splitmybills/Person.java
@@ -1,9 +1,11 @@
 package com.millstein.mark.splitmybills;
 
 /**
- * Created by mark on 2/15/15.
+ * Created by mark on 
+ * * 2/15/15.
  */
 public class Person {
+    private String name;
     private int singles;
     private int fives;
     private int tens;
@@ -11,12 +13,69 @@ public class Person {
     private int fifties;
     private int hundreds;
 
-    public Person(int singles, int fives, int tens, int twenties, int fifties, int hundreds) {
+    public Person(String name, int singles, int fives, int tens, int twenties, int fifties, int hundreds) {
+        this.name = name;
         this.singles = singles;
         this.fives = fives;
         this.tens = tens;
         this.twenties = twenties;
         this.fifties = fifties;
         this.hundreds = hundreds;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSingles(int singles) {
+        this.singles = singles;
+    }
+
+    public void setFives(int fives) {
+        this.fives = fives;
+    }
+
+    public void setTens(int tens) {
+        this.tens = tens;
+    }
+
+    public void setTwenties(int twenties) {
+        this.twenties = twenties;
+    }
+
+    public void setFifties(int fifties) {
+        this.fifties = fifties;
+    }
+
+    public void setHundreds(int hundreds) {
+        this.hundreds = hundreds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSingles() {
+        return singles;
+    }
+
+    public int getFives() {
+        return fives;
+    }
+
+    public int getTens() {
+        return tens;
+    }
+
+    public int getTwenties() {
+        return twenties;
+    }
+
+    public int getFifties() {
+        return fifties;
+    }
+
+    public int getHundreds() {
+        return hundreds;
     }
 }
